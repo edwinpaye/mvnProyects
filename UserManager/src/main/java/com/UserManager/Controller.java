398dff97430390819dca3fe9f75773043ccb3d70
edwinpaye <baratheonts@gmail.com>
2018-09-04 22:48:33 -0400

    add class Controller

---
+++ UserManager/src/main/java/com/UserManager/Controller.java
@@ -0,0 +1,17 @@
+package com.UserManager;
+
+public class Controller {
+    
+    public Controller(){
+        
+    }
+    
+    public void start(UserManager newUserManager){
+//        EditionWindow editionWindow = new EditionWindow(new UserManager(newConectionSql));
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new EditionWindow(newUserManager).setVisible(true);
+            }
+        });
+    }
+}
