75fceb0867d8c796368c4818ea02fee4dd204ca4
edwinpaye <baratheonts@gmail.com>
2018-09-08 00:18:27 -0400

    add getTable

--- UserManager/src/main/java/com/UserManager/ConectionSql.java
+++ UserManager/src/main/java/com/UserManager/ConectionSql.java
@@ -4,12 +4,26 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConectionSql {
-    private String path, user,password;
     
-    public ConectionSql(String path,String user,String password) {
-        this.path=path;
-        this.user=user;
+    private String path, user, password, table;
+    
+    public ConectionSql(String path, String table,String user,String password) {
+        this.path = path;
+        this.user = user;
         this.password = password;
+        this.table = table;
+    }
+    
+    public String getTable(){
+        System.out.println("funciona"+table);
+        return table;
+    }
+    
+    public ConectionSql(DBConfig newDBConfig) {
+        this.path = "jdbc:mysql://"+newDBConfig.getServer()+":"+newDBConfig.getPort()+"/"+newDBConfig.getBaseDato();
+        this.user = newDBConfig.getUser();
+        this.password = newDBConfig.getPassword();
+        this.table = newDBConfig.getDBTable();
     }
     
     public Connection Connect() {
@@ -22,5 +36,4 @@ public class ConectionSql {
         }
         return null;
     }
-//
 }
