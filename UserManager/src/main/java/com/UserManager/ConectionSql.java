7bf2334c23df932cb8d855e6443fb29ae1445109
edwinpaye <baratheonts@gmail.com>
2018-09-15 00:48:13 -0400

    refactoring corrections

--- UserManager/src/main/java/com/UserManager/ConectionSql.java
+++ UserManager/src/main/java/com/UserManager/ConectionSql.java
@@ -8,7 +8,7 @@ public class ConectionSql {
     private String path;
     private String user;
     private String password;
-    private String dataBase;
+    private String dataBaseDefault;
     
     public ConectionSql(String path, String user,String password) {
         this.path = path;
@@ -20,12 +20,12 @@ public class ConectionSql {
         this.path = "jdbc:mysql://"+newDBConfig.getServer()+":"+newDBConfig.getPort();
         this.user = newDBConfig.getUser();
         this.password = newDBConfig.getPassword();
-        this.dataBase = newDBConfig.getBaseDato();
+        this.dataBaseDefault = newDBConfig.getBaseDato();
     }
     
     public Connection connect() {
         try{
-            return DriverManager.getConnection(path+"/"+dataBase, user, password);
+            return DriverManager.getConnection(path+"/"+dataBaseDefault, user, password);
         }catch (Exception e) {
             System.out.println("Unable to establish connection " + e.getMessage());
             e.getStackTrace();
@@ -33,9 +33,9 @@ public class ConectionSql {
         return null;
     }
     
-    public Connection connectDataBase(String newDataBase) {
+    public Connection connectDataBase(String dataBase) {
         try{
-            return DriverManager.getConnection(path+"/"+newDataBase, user, password);
+            return DriverManager.getConnection(path+"/"+dataBase, user, password);
         }catch (Exception e) {
             System.out.println("Unable to establish data Base connection " + e.getMessage());
             e.getStackTrace();
