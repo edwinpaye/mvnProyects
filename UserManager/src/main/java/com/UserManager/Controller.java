6be58bb43253d5e3d98f7886e285bdaa4a293a99
edwinpaye <baratheonts@gmail.com>
2018-09-14 22:59:42 -0400

    refactoring process

--- UserManager/src/main/java/com/UserManager/Controller.java
+++ UserManager/src/main/java/com/UserManager/Controller.java
@@ -3,22 +3,26 @@ package com.UserManager;
 public class Controller {
 
     public Controller(){}
+    
+    private Controller getController(){
+        return this;
+    }
 
     public void start(UserManager newUserManager){
 
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new DataBaseSelector(newUserManager).setVisible(true);
+                new DataBaseSelector(newUserManager, getController()).setVisible(true);
             }
         });
         
-//        if (newUserManager.getConnectSql().getDataBase() != null && newUserManager.getConnectSql().getTable() != null) {
-//            java.awt.EventQueue.invokeLater(new Runnable() {
-//                public void run() {
-//                    new EditionWindow(newUserManager).setVisible(true);
-//                }
-//            });    
-//        }
-        
+    }
+    
+    public void runEditionWindow(String newDataBase, String newTable, UserManager newUserManager){
+            java.awt.EventQueue.invokeLater(new Runnable() {
+                public void run() {
+                    new EditionWindow(newUserManager, newDataBase, newTable).setVisible(true);
+                }
+            });            
     }
 }
