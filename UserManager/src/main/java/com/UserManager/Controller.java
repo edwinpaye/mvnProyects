411b6bda18dd44fa18013fdfe3d3d28636f6760b
edwinpaye <baratheonts@gmail.com>
2018-09-16 23:13:56 -0400

    fix Controller

--- UserManager/src/main/java/com/UserManager/Controller.java
+++ UserManager/src/main/java/com/UserManager/Controller.java
@@ -3,7 +3,7 @@ package com.UserManager;
 public class Controller {
 
     public Controller(){}
-    
+
     private Controller getController(){
         return this;
     }
@@ -15,14 +15,14 @@ public class Controller {
                 new DataBaseSelector(newUserManager, getController()).setVisible(true);
             }
         });
-        
+
     }
-    
+
     public void runEditionWindow(String newDataBase, String newTable, UserManager newUserManager){
-            java.awt.EventQueue.invokeLater(new Runnable() {
-                public void run() {
-                    new EditionWindow(newUserManager, newDataBase, newTable).setVisible(true);
-                }
-            });            
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new EditionWindow(newUserManager, newDataBase, newTable).setVisible(true);
+            }
+        });            
     }
 }
